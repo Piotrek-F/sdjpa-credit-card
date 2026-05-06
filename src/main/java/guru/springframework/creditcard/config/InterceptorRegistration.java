ea5759ab6df0c1ac7561cc091fc973b0a218106f
Piotr Sadłowski <piotr.f.sadlowski@gmail.com>
2026-05-06 15:01:31 +0200

    End code for lesson 194 "Remove Listener Example" - commenting out some fragments of the code to disable EncryptionInterceptor.

--- src/main/java/guru/springframework/creditcard/config/InterceptorRegistration.java
+++ src/main/java/guru/springframework/creditcard/config/InterceptorRegistration.java
@@ -1,20 +1,17 @@
 package guru.springframework.creditcard.config;
 
-import guru.springframework.creditcard.interceptors.EncryptionInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.Map;
 
-@Configuration
+//@Configuration
 public class InterceptorRegistration implements HibernatePropertiesCustomizer {
 
-    @Autowired
-    EncryptionInterceptor interceptor;
+    // @Autowired
+    // EncryptionInterceptor interceptor;
 
     @Override
     public void customize(Map<String, Object> hibernateProperties) {
-        hibernateProperties.put("hibernate.session_factory.interceptor", interceptor);
+    //    hibernateProperties.put("hibernate.session_factory.interceptor", interceptor);
     }
 }
