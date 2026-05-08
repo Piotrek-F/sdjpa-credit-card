8390a08306b97d095e1396ce7ecb210c8f64b1f5
Piotr Sadłowski <piotr.f.sadlowski@gmail.com>
2026-05-08 22:03:20 +0200

    End code for lesson 197 "Implement Hibernate Encryption Listeners" - implementing AbstractEncyptionListener and extending listeners with it.

--- src/main/java/guru/springframework/creditcard/listeners/PostLoadListener.java
+++ src/main/java/guru/springframework/creditcard/listeners/PostLoadListener.java
@@ -1,14 +1,21 @@
 package guru.springframework.creditcard.listeners;
 
+import guru.springframework.creditcard.services.EncryptionService;
 import org.hibernate.event.spi.PostLoadEvent;
 import org.hibernate.event.spi.PostLoadEventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PostLoadListener implements PostLoadEventListener {
+public class PostLoadListener extends AbstractEncyptionListener implements PostLoadEventListener {
+
+    public PostLoadListener(EncryptionService encryptionService) {
+        super(encryptionService);
+    }
 
     @Override
     public void onPostLoad(PostLoadEvent event) {
         System.out.println("In Post Load");
+
+        this.decrypt(event.getEntity());
     }
 }
